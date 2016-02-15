32dfae253bd0cbb6f9d66ff16fe182d15f255686
philippepego95 <dam.pgonzalez@institutoserpis.org>
2016-02-15 12:26:21 +0100

    Calculadora amb try i catch

--- calculadora/app/src/main/java/com/mycompany/calculadora/MainActivity.java
+++ calculadora/app/src/main/java/com/mycompany/calculadora/MainActivity.java
@@ -11,6 +11,9 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,57 +23,78 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        txtn1=(EditText)findViewById(R.id.txtn1);
-        txtn2=(EditText)findViewById(R.id.txtn2);
-        btnS=(Button)findViewById(R.id.btnSuma);
-        btnR=(Button)findViewById(R.id.btnResta);
-        btnM=(Button)findViewById(R.id.btnMulti);
-        btnD=(Button)findViewById(R.id.btnDiv);
-        txtR=(TextView)findViewById(R.id.txtRes);
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
-        btnS.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                double num1= Double.parseDouble(txtn1.getText().toString());
-                double num2= Double.parseDouble(txtn2.getText().toString());
-                double s=num1+num2;
-                txtR.setText("Resultat: "+s);
-            }
-        });
+            txtn1 = (EditText) findViewById(R.id.txtn1);
+            txtn2 = (EditText) findViewById(R.id.txtn2);
+            btnS = (Button) findViewById(R.id.btnSuma);
+            btnR = (Button) findViewById(R.id.btnResta);
+            btnM = (Button) findViewById(R.id.btnMulti);
+            btnD = (Button) findViewById(R.id.btnDiv);
+            txtR = (TextView) findViewById(R.id.txtRes);
 
-        btnM.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                double num1= Double.parseDouble(txtn1.getText().toString());
-                double num2= Double.parseDouble(txtn2.getText().toString());
-                double s=num1*num2;
-                txtR.setText("Resultat: "+s);
-            }
-        });
+            btnS.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    try {
+                        double num1 = Double.parseDouble(txtn1.getText().toString());
+                        double num2 = Double.parseDouble(txtn2.getText().toString());
+                        double s = num1 + num2;
+                        txtR.setText("Resultat: " + s);
+                    }catch (NumberFormatException e){
+                        Toast.makeText(MainActivity.this, R.string.only_number_text,Toast.LENGTH_SHORT).show();
+                    }
 
-        btnR.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                double num1= Double.parseDouble(txtn1.getText().toString());
-                double num2= Double.parseDouble(txtn2.getText().toString());
-                double s=num1-num2;
-                txtR.setText("Resultat: "+s);
-            }
-        });
+                }
+            });
 
-        btnD.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                double num1= Double.parseDouble(txtn1.getText().toString());
-                double num2= Double.parseDouble(txtn2.getText().toString());
-                double s=num1/num2;
-                txtR.setText("Resultat: "+s);
-            }
-        });
+            btnM.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    try {
+                    double num1 = Double.parseDouble(txtn1.getText().toString());
+                    double num2 = Double.parseDouble(txtn2.getText().toString());
+                    double s = num1 * num2;
+                    txtR.setText("Resultat: " + s);
+                    }catch (NumberFormatException e){
+                        Toast.makeText(MainActivity.this, R.string.only_number_text,Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+            btnR.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    try {
+
+                        double num1 = Double.parseDouble(txtn1.getText().toString());
+                        double num2 = Double.parseDouble(txtn2.getText().toString());
+                        double s = num1 - num2;
+                        txtR.setText("Resultat: " + s);
+
+                    } catch (NumberFormatException e) {
+                        Toast.makeText(MainActivity.this, R.string.only_number_text, Toast.LENGTH_SHORT).show();
+                    }
+                } });
+
+            btnD.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                        try {
+
+                    double num1 = Double.parseDouble(txtn1.getText().toString());
+                    double num2 = Double.parseDouble(txtn2.getText().toString());
+                    double s = num1 / num2;
+                    txtR.setText("Resultat: " + s);
+                        }catch (NumberFormatException e){
+                            Toast.makeText(MainActivity.this, R.string.only_number_text,Toast.LENGTH_SHORT).show();
+                        }}
+            });
+
+        }
     }
 
 
-}
+
