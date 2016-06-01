8d85d7f78f1a6706572d88f4a9623223da9766a8
philippepego95 <dam.pgonzalez@institutoserpis.org>
2016-06-01 08:51:44 +0200

    ACategoria modificado

--- ACategoria/app/src/main/java/org/institutoserpis/ed/acategoria/MainActivity.java
+++ ACategoria/app/src/main/java/org/institutoserpis/ed/acategoria/MainActivity.java
@@ -2,6 +2,10 @@ package org.institutoserpis.ed.acategoria;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        dbpruebahelper.inic(this);
+
+        List<Categoria> categorias = dbpruebahelper.getInstance().getCategorias();
+
+        ListView listview = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<Categoria> arrayAdapter=new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                categorias
+        );
+
+            listview.setAdapter(arrayAdapter);
+
+
     }
 }
